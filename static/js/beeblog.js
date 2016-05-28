5b8f83c367c7c6a3bce78929d6eb269b24144710
YanGeng <18516054923@163.com>
2016-05-28 15:39:38 +0800

    new update

--- static/js/beeblog.js
+++ static/js/beeblog.js
@@ -1,4 +1,6 @@
-<!-- 新 Bootstrap 核心 CSS 文件 -->
++function ($) {
+  'use strict';    
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <!-- 可选的Bootstrap主题文件（一般不用引入） -->
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
@@ -6,3 +8,4 @@
     <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
     <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+}(jQuery);
